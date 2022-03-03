b5a7e6741fa44a8b058b90eca20ad5ab557454e6
Alina Kornienko <a_kornienko@ukr.net>
2022-03-03 16:10:24 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,8 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", createGalleryItems);
+
